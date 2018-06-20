960d1992986358229a9fccc8f6640d385c8103e3
Luigi DI Rado <40144238+gi-diana@users.noreply.github.com>
2018-06-20 15:23:45 +0200

    Stream Poin Gray
    Stream a Point Grey camera over an IP specified and a UDP port

---
+++ opencv_gstreamer.cpp
@@ -0,0 +1,91 @@
+#include "FlyCapture2.h"
+#include <opencv2/core/core.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <iostream>
+
+using namespace FlyCapture2;
+
+int main()
+{
+    Error error;
+    Camera camera;
+    CameraInfo camInfo;
+
+    // Connect the camera
+    error = camera.Connect( 0 );
+    if ( error != PGRERROR_OK )
+    {
+        std::cout << "Failed to connect to camera" << std::endl;     
+        return false;
+    }
+
+    // Get the camera info and print it out
+    error = camera.GetCameraInfo( &camInfo );
+    if ( error != PGRERROR_OK )
+    {
+        std::cout << "Failed to get camera info from camera" << std::endl;     
+        return false;
+    }
+    std::cout << camInfo.vendorName << " "
+              << camInfo.modelName << " " 
+              << camInfo.serialNumber << std::endl;
+	
+    error = camera.StartCapture();
+    if ( error == PGRERROR_ISOCH_BANDWIDTH_EXCEEDED )
+    {
+        std::cout << "Bandwidth exceeded" << std::endl;     
+        return false;
+    }
+    else if ( error != PGRERROR_OK )
+    {
+        std::cout << "Failed to start image capture" << std::endl;     
+        return false;
+    } 
+cv::VideoWriter out("appsrc ! videoconvert ! video/x-raw,format=YUY2,width=1280,height=1024, framerate=50/1 ! jpegenc ! rtpjpegpay ! udpsink host=10.0.0.102 port=5000", 1800,0,50, cv::Size(1280,1024), true);
+
+if (out.isOpened()){
+	puts("Pipeline Opened");
+}
+
+else {
+	puts("Pipeline Broken");
+}
+char key = 0;
+int lost =0;
+while(key != 'q'){
+
+	Image raw;
+	Error error = camera.RetrieveBuffer(&raw);
+	if (error != PGRERROR_OK){
+		std::cout << "network loss frame" << std::endl;
+                lost++;
+		continue;
+	}
+	printf("Frames lost: %d \n", lost);
+	Image rgb;
+	raw.Convert( FlyCapture2::PIXEL_FORMAT_BGR, &rgb );
+	
+	unsigned int row = (double)rgb.GetReceivedDataSize()/(double)rgb.GetRows();
+	
+	cv::Mat image = cv::Mat(rgb.GetRows(), rgb.GetCols(), CV_8UC3, rgb.GetData(),row);
+	
+	cv::imshow("image",image);
+        
+	out.write(image);
+	key= cv::waitKey(30);
+}
+
+ error = camera.StopCapture();
+    if ( error != PGRERROR_OK )
+    {
+        // This may fail when the camera was removed, so don't show 
+        // an error message
+    }  
+
+    camera.Disconnect();
+
+    return 0;
+}    
+
+
+
